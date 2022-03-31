ef1841251e9c64c56c8448f02f165528aeb67994
Rhys Johns <rhysjohnsdev@gmail.com>
2022-03-31 22:17:12 +1100

    - Implemented the basic component - Users can now quit by pressing Ctrl+C or Q

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"os"
+)
+
+type model struct {
+	choices []string
+}
+
+func initalModel() model {
+	return model{}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	return "Welcome to the Observation Deck! v0.1"
+}
 
 func main() {
-	fmt.Println("Welcome to the Observation Deck!")
+	p := tea.NewProgram(initalModel())
+	if err := p.Start(); err != nil {
+		fmt.Printf("The deck is burning, error: %v", err)
+		os.Exit(1)
+	}
 }
